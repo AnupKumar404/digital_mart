f6c958a72283329129b956005590905917b1e2d2
er.anupkumar404 <anup67904@gmail.com>
2026-08-05 19:07:19 +0530

    fix an issue in config file

--- src/services/cartApi.js
+++ src/services/cartApi.js
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import { httpClient } from "../configs/httpClient.js";
+import { httpClient } from "../configs/HttpClient.js";
 
 export const cartApi = createApi({
     reducerPath: "cartApi",
