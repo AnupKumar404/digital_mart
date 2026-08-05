6e53bf9136ccd675778628926414a3e949733334
er.anupkumar404 <anup67904@gmail.com>
2026-08-05 18:37:03 +0530

    modify import line

--- src/services/cartApi.js
+++ src/services/cartApi.js
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import { httpClient } from "../configs/httpClient";
+import { httpClient } from "../configs/httpClient.js";
 
 export const cartApi = createApi({
     reducerPath: "cartApi",
