077852fe5521178416d58108c785e002feee12c1
er.anupkumar404 <anup67904@gmail.com>
2026-08-08 19:07:59 +0530

    changes url path to origin server

--- src/configs/HttpClient.js
+++ src/configs/HttpClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'ec2-43-205-54-139.ap-south-1.compute.amazonaws.com:8080';
+const baseURL = 'ec2-43-205-54-139.ap-south-1.compute.amazonaws.com';
 // const baseURL = 'http://43.205.54.139:8080';
 
 export const httpClient = axios.create({
