082be596bbb25ba6ddee5755a0fb736043c40cbb
er.anupkumar404 <anup67904@gmail.com>
2026-08-08 19:22:45 +0530

    add vercel config file

--- src/configs/HttpClient.js
+++ src/configs/HttpClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'ec2-43-205-54-139.ap-south-1.compute.amazonaws.com';
+const baseURL = '';
 // const baseURL = 'http://43.205.54.139:8080';
 
 export const httpClient = axios.create({
