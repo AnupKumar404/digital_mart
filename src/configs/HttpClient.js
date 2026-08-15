1b402d191909c8121b96cd92d552bf0b52b06ac0
er.anupkumar404 <anup67904@gmail.com>
2026-08-15 20:45:51 +0530

    some configs for cors

--- src/configs/HttpClient.js
+++ src/configs/HttpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseURL = 'http://localhost:8080';
-const baseURL = 'http://dailyveggies4u.com';
+const baseURL = 'https://dailyveggies4u.com';
 
 export const httpClient = axios.create({
     baseURL: baseURL,
