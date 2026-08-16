cb9b371fa0a9436362be0900138c706a4e116766
er.anupkumar404 <anup67904@gmail.com>
2026-08-16 10:59:20 +0530

    base url routing done

--- src/services/cartApi.js
+++ src/services/cartApi.js
@@ -4,7 +4,7 @@ import { httpClient } from "../configs/HttpClient.js";
 export const cartApi = createApi({
     reducerPath: "cartApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: httpClient.baseURL,
+        baseURL: httpClient.baseURL,
     }),
 
     tagTypes: ["Cart", "Product"],
